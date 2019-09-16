26e87b931ec414da3f549939783c145caea1f77d
jaguarman9 <49654802+jaguarman9@users.noreply.github.com>
2019-09-16 15:31:27 -0700

    1

--- project-socket/socket-base/index.js
+++ project-socket/socket-base/index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
